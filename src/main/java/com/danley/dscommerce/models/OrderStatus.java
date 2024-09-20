d12f6e81b1966635ba0ba8f7f109e6196181e0d2
rootdanley <danleyalves@hotmail.com>
2024-09-20 13:28:48 -0300

    feat: payment entity

---
+++ src/main/java/com/danley/dscommerce/models/OrderStatus.java
@@ -0,0 +1,10 @@
+package com.danley.dscommerce.models;
+
+public enum OrderStatus {
+    WAIT_PAYMENT,
+    PAID,
+    SHIPPED,
+    DELIVERED,
+    CANCELED,
+
+}
---
+++ src/main/java/com/danley/dscommerce/models/Payment.java
@@ -0,0 +1,41 @@
+package com.danley.dscommerce.models;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+
+    @OneToOne
+    @MapsId
+    private Order order;
+
+    public Payment(Long id, Instant moment, Order order) {
+        this.id = id;
+        this.moment = moment;
+        this.order = order;
+    }
+
+    public Payment() {}
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+}
